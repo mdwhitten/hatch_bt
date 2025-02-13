fb2a8fd8642626fab985cd9c365f475f419b848f
Michael Whitten <michael.whitten@rsa.rohde-schwarz.com>
2025-02-13 07:45:57 -0600

    Added handling to ensure connection is closed on setup error

--- custom_components/hatch_bt/__init__.py
+++ custom_components/hatch_bt/__init__.py
@@ -36,15 +36,19 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     if not device.connected:
         raise ConfigEntryNotReady(f"Failed to connect to: {ble_device.address}")
 
-    coordinator = HatchBTUpdateCoordinator(hass, _LOGGER, device, ble_device, entry.title, entry.unique_id)
+    try:
+        coordinator = HatchBTUpdateCoordinator(hass, _LOGGER, device, ble_device, entry.title, entry.unique_id)
 
-    await coordinator.async_config_entry_first_refresh()
+        await coordinator.async_config_entry_first_refresh()
 
-    hass.data[DOMAIN][entry.entry_id] = coordinator
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+        hass.data[DOMAIN][entry.entry_id] = coordinator
+        await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
-    return True
-
+        return True
+    except:
+        _LOGGER.exception("Exception occured during setup; closing connection")
+        await device.disconnect()
+        raise
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
